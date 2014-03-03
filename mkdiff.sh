127e12a3388a71e489b0fc1610838b9147cfdc97
Duane Johnson <duane.johnson@gmail.com>
2014-03-03 12:05:21 -0700

    Add mkdiff script to compare TLW-1816 and -1819

---
+++ mkdiff.sh
@@ -0,0 +1,32 @@
+cat <<-HTML
+<html>
+<head>
+  <style>
+  span.delete { text-decoration: line-through; color: red; }
+  span.insert { color: blue; font-weight: bold; }
+  body { margin: 1em 10em; }
+  </style>
+</head>
+<body>
+HTML
+
+# --aggregate-changes
+dwdiff \
+  -i \
+  -A best \
+  -w '<span class="delete">' -x '</span>' \
+  -y '<span class="insert">' -z '</span>' \
+  <(cat 'pseudo_biblical/The Late War - Gilbert Hunt - 1816.md' | 
+    sed -n '/\[BEGIN\]/,/\[END\]/p' |
+    sed -E 's/^[0-9]+ *//' |
+    sed -E 's/\[[^]]+\]//' |
+    ruby -e 'puts $stdin.read.gsub(/\n\s*\n\s*\n/m, "\n\n").gsub("\n", "<br>")') \
+  <(cat 'pseudo_biblical/The Late War - Gilbert Hunt - 1819.md' | 
+    sed -n '/\[BEGIN\]/,/\[END\]/p' |
+    sed -E 's/^[0-9]+ *//' |
+    sed -E 's/\[[^]]+\]//' |
+    ruby -e 'puts $stdin.read.gsub(/\n\s*\n\s*\n/m, "\n\n").gsub("\n", "<br>")')
+
+cat <<-HTML
+</body>
+HTML
